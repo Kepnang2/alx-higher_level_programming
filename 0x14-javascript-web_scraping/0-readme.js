6443dd152a31122b4d6d258a441a80d8e2c6b8c7
Kepnang2 <kepemafa@yahoo.fr>
2023-02-09 15:42:52 +0000

    javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// read and print a file
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
